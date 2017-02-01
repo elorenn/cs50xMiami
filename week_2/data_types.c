c31905dcf4d306dae7976717cd328dec2b839795
Lorena.Sarasua <lorena.sarasua@gmail.com>
2017-02-01 23:55:34 +0000

    wooooo, data types in C

---
+++ week_2/data_types.c
@@ -0,0 +1,14 @@
+// wed 2/1/17 week 2
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main () {
+    
+    string aString = "Lorena";
+    char aChr = aString[2];
+    // this should be "r"
+    printf("%c\n",aChr);
+    
+    return 0;
+}
