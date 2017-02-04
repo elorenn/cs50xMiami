b2e73a155274cf50c59f0d3b6179cd617342a8c2
Lorena.Sarasua <lorena.sarasua@gmail.com>
2017-02-04 01:19:21 +0000

    problem set 1 setup

--- week_2/Me/string.c
+++
@@ -1,5 +0,0 @@
-// mon 1/30/17 week 2
-
-#include <stdio.h>
-#include <cs50.h>
-#include <string.h>
---
+++ week_2/pset1/water.c
@@ -0,0 +1,27 @@
+/* 
+a program that prompts the user for the length of his or her shower in minutes (as a positive integer) 
+and then prints the equivalent number of bottles of water (as an integer) per the sample output below, 
+wherein underlined text represents some user’s input.
+For simplicity, you may assume that the user will input a positive integer, so no need for error-checking
+(or any loops) this time! And no need to worry about overflow!
+
+minutes: 10
+bottles: 120
+
+And if you’d like to play with the staff’s own implementation of water within CS50 IDE, you may execute the below:
+~cs50/pset1/water
+*/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main( ) {
+    
+    printf( "minutes: ");
+    // string str = GetString();
+    // printf("Select (U)pper, (L)ower, or (S)wap case: ");
+    // char selection = GetChar();
+    
+
+    return 0; // good practice to include this for feedback
+}
