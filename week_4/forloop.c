cf26613c0991d2d98252ea21eb47b1fdf62a51f8
Lorena.Sarasua <lorena.sarasua@gmail.com>
2017-02-20 03:32:10 +0000

    countdown with for loop

---
+++ week_4/forloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void) 
+{
+    for (int i = 5; i > 0; i--) 
+    {
+        printf("%d\n", i);
+    }
+    
+    printf("Blast off!\n");
+    return 0;
+}
